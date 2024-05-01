33a1b054b0abbb8819153ea9a4e4e0e15fcefada
othmane bakkass <othmanebakkass@gmail.com>
2024-05-01 19:42:05 +0100

    refactor: define relations for conversation table

--- backend/app/Models/Conversation.php
+++ backend/app/Models/Conversation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Conversation extends Model
 {
@@ -14,5 +15,16 @@ class Conversation extends Model
         'user_id_2',
         'last_message_id'
     ];
+
+
+    function participant_one(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id_1');
+    }
+
+    function participant_two(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id_2');
+    }
 
 }
