4f2f9b1662b9d83092d6b51ba1ee58519de884b8
othmane bakkass <othmanebakkass@gmail.com>
2024-05-01 19:43:06 +0100

    refactor: define relations for message table

--- backend/app/Models/Message.php
+++ backend/app/Models/Message.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Message extends Model
 {
@@ -15,5 +17,25 @@ class Message extends Model
         'group_id',
         'message'
     ];
+
+    function reciever(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'reciever_id');
+    }
+
+    function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class, 'group_id');
+    }
+
+    function attachements(): HasMany
+    {
+        return $this->hasMany(MessageAttachment::class);
+    }
 
 }
